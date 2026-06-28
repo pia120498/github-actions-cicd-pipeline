daa2cc66c7406e2f9b1abd901f37a2bfdb9e2e61
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-06-28 15:07:58 +0530

    feat: implement task management interface

--- app/app.py
+++ app/app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    tasks = [
+        {"name": "Deploy Docker Container", "completed": False},
+        {"name": "Configure GitHub Actions", "completed": True},
+        {"name": "Deploy to Kubernetes", "completed": False},
+    ]
+
+    return render_template("index.html", tasks=tasks)
 
 
 if __name__ == "__main__":
